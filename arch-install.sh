ee27087aabe47d197e2606e607ff5d2237cdfe23
Sri harsha <40120124+DerpGusta@users.noreply.github.com>
2020-08-17 01:53:10 +0530

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -2,10 +2,10 @@
 
 if [[ ! $(curl -Is http://www.google.com/ | head -n 1) =~ "200 OK" ]]; then
   echo "Your Internet seems broken. Press Ctrl-C to abort or enter to continue."
-  read
+  exit
 fi
 
-rootpass= $(read -sp 'Enter password for root:') 
+read -rsp 'Enter password for root:' rootpass
 timedatectl set-ntp true
 wipefs -a /dev/sda
 sgdisk -o /dev/sda
@@ -22,50 +22,55 @@ mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 swapon /dev/sda3
 
-pacman -S --needed --noconfirm --overwrite="*" pacman-contrib
-echo "\n\nWait for rankmirrors to complete!!\n\n"
+pacman -S --needed --noconfirm pacman-contrib
+echo -e "\n\nWait for rankmirrors to complete!!\n\n"
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.orig
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.orig >/etc/pacman.d/mirrorlist
 
-pacstrap /mnt base base-devel linux linux-firmware
+pacstrap /mnt base base-devel linux linux-firmware open-vm-tools xf86-video-vmware \
+lxdm xf86-input-vmmouse
+
 genfstab -U /mnt >> /mnt/etc/fstab
-root_uuid="$(blkid "/dev/sda2" | cut -d ' ' -f 2 | cut -d '"' -f 2)"
+root_uuid=$(lsblk /dev/sda2 -no uuid)
 
-cp -L /etc/resolv.conf "/mnt/etc/resolv.conf"
+cp -L /etc/resolv.conf /mnt/etc/resolv.conf
 
-cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/ 
+cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/
 
-arch-chroot /mnt /bin/bash <<EOF
-ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
-$LOCALE="en_US.UTF-8"
-sed -i '/$LOCALE/s/^#//' /etc/locale.gen
-echo "LANG=$LOCALE" > /etc/locale.conf
-echo "KEYMAP=us" > /etc/vconsole.conf
-locale-gen
-cat "tytoalba" > /etc/hostname
-mkinitcpio -P
-passwd $rootpass
-bootctl install
-cat >> "/boot/loader/entries/arch.conf" <<BOOTEND
+echo "root:$rootpass" | chpasswd -R /mnt 
+
+ln -sf /mnt/usr/share/zoneinfo/Asia/Kolkata /mnt/etc/localtime
+LOCALE="en_US.UTF-8"
+sed -i "/$LOCALE/s/^#//" /mnt/etc/locale.gen
+echo "LANG=$LOCALE" > /mnt/etc/locale.conf
+echo "KEYMAP=us" > /mnt/etc/vconsole.conf
+cat "tytoalba" > /mnt/etc/hostname
+bootctl install --boot-path=/mnt/boot
+touch /mnt/boot/loader/entries/
+cat >> "/mnt/boot/loader/entries/arch.conf" << BOOTEND
 title   Arch Linux
 linux   /vmlinuz-linux
 initrd    /initramfs-linux.img
 options   root=UUID=$root_uuid rw
 BOOTEND
-systemctl enable systemd-networkd
-systemctl enable systemd-resolved
-network_file="/etc/systemd/netowrk/20-wired.conf"
+
+network_file="/mnt/etc/systemd/network/20-wired.conf"
 if [[ -f $network_file ]]; then
 sed -i '/\[Network\]/a DNS=1.0.0.1' $network_file
+fi
+
 curl -O https://blackarch.org/strap.sh
-echo 9c15f5d3d6f3f8ad63a6927ba78ed54f1a52176b strap.sh | sha1sum -c
+echo 9c15f5d3d6f3f8ad63a6927ba78ed54f1a52176b strap.sh | sha1sum -c - || exit 1
 chmod +x strap.sh
-./strap.sh
- pacman -S open-vm-tools xf86-video-vmware \
-lxdm xf86-input-vmmouse --overwrite='*' --needed --noconfirm
+arch-chroot /mnt /bin/bash < strap.sh
+
+arch-chroot /mnt /bin/bash <<EOF
+locale-gen
+systemctl enable systemd-networkd
 systemctl enable vmware-vmblock-fuse.service vmtoolsd.service
 systemctl enable lxdm.service
 EOF
+
 umount -R /mnt
 
-echo "Done! Now all you have to do is run the darbs.sh after reboot" 
+echo "Done! Now all you have to do is run the darbs.sh after reboot"
