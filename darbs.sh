0fbb33138d8a2882a3b6e71e33762e7f85ddba69
Sri harsha <40120124+DerpGusta@users.noreply.github.com>
2020-08-17 23:09:43 +0530

    Update darbs.sh

--- darbs.sh
+++ darbs.sh
@@ -13,22 +13,17 @@ error() { clear; printf "ERROR:\\n%s\\n" "$1"; exit;}
 getuserandpass() { \
 	# Prompts user for password.
 	name="derp"
-	pass1=$(dialog --no-cancel --passwordbox --insecure "Enter a password for that user." 10 60 3>&1 1>&2 2>&3 3>&1)
-	pass2=$(dialog --no-cancel --passwordbox --insecure "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+	read -spr 'Enter a password for that user.' pass1
+	read -spr 'Retype password.' pass2
 	while ! [ "$pass1" = "$pass2" ]; do
 		unset pass2
-		pass1=$(dialog --no-cancel --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
-		pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+		read -spr 'Enter a password for that user.' pass1
+	        read -spr 'Retype password.' pass2
 	done ;}
-
-usercheck() { \
-	! (id -u "$name" >/dev/null) 2>&1 ||
-	dialog --colors --title "WARNING!" --yes-label "CONTINUE" --no-label "No wait..." --yesno "The user \`$name\` already exists on this system. DARBS can install for a user already existing, but it will \\Zboverwrite\\Zn any conflicting settings/dotfiles on the user account.\\n\\nDARBS will \\Zbnot\\Zn overwrite your user files, documents, videos, etc., so don't worry about that, but only click <CONTINUE> if you don't mind your settings being overwritten.\\n\\nNote also that DARBS will change $name's password to the one you just gave." 14 70
-	}
 
 adduserandpass() { \
 	# Adds user `$name` with password $pass1.
-	dialog --infobox "Adding user \"$name\"..." 4 50
+	echo "Adding user \"$name\"..."
 	groupadd "$name" >/dev/null 2>&1
 	useradd -m -g wheel -s /bin/bash "$name" >/dev/null 2>&1 ||
 	usermod -a -G wheel,users,video,audio,$name "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
@@ -37,7 +32,7 @@ adduserandpass() { \
 	unset pass1 pass2 ;}
 
 refreshkeys() { \
-	dialog --infobox "Refreshing Arch Keyring..." 4 40
+	echo -e "\n REFRESHING ARCHLINUX KEYRING"
 	pacman --noconfirm -Sy archlinux-keyring >/dev/null 2>&1
 	}
 
@@ -54,14 +49,13 @@ manualinstall() { # Installs $1 manually if not installed. Used only for AUR hel
 	cd /tmp || exit;}
 
 maininstall() { # Installs all needed programs from main repo.
-	dialog --title "DARBS Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 5 70
 	installpkg "$1"
 	}
 
 gitmakeinstall() {
 	progname="$(basename "$1" .git)"
 	dir="$repodir/$progname"
-	dialog --title "DARBS Installation" --infobox "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 5 70
+	echo -e "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2\n"
 	sudo -u "$name" git clone --depth 1 "$1" "$dir" >/dev/null 2>&1 || { cd "$dir" || return ; sudo -u "$name" git pull --force origin master;}
 	cd "$dir" || exit
 	make >/dev/null 2>&1
@@ -69,7 +63,7 @@ gitmakeinstall() {
 	cd /tmp || return ;}
 
 aurinstall() { \
-	dialog --title "DARBS Installation" --infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 5 70
+	echo -e "Installing \`$1\` ($n of $total) from the AUR. $1 $2\n"
 	echo "$aurinstalled" | grep "^$1$" >/dev/null 2>&1 && return
 	sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1
 	}
@@ -89,19 +83,14 @@ installationloop() { \
 	done < /tmp/progs.csv ;}
 
 putgitrepo() { # Downloads a gitrepo $1 and places the symbolic links to files in $2 using stow
-	dialog --infobox "Downloading and installing config files..." 4 60
 	sudo -u "$name" git clone --depth 1 "$1" "$dir" >/dev/null 2>&1
 	sudo -u "$name" sh -c "cd $dir/dotfiles && stow \"$(ls -d /*)\""
 	}
 
-systembeepoff() { dialog --infobox "Getting rid of that retarded error beep sound..." 10 50
+systembeepoff() { echo "Getting rid of that retarded error beep sound...\n"
 	rmmod pcspkr
 	echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf ;}
 
-finalize(){ \
-	dialog --infobox "Preparing welcome message..." 4 50
-	dialog --title "All done!" --msgbox "Congrats! Provided there were no hidden errors, the script completed successfully and all the programs and configuration files should be in place.\\n\\nTo run the new graphical environment, log out and log back in as your new user, then run the command \"startx\" to start the graphical environment (it will start automatically in tty1).\\n\\n.t Derp" 12 80
-	}
 
 ### THE ACTUAL SCRIPT ###
 
@@ -112,9 +101,6 @@ installpkg dialog || error "Are you sure you're running this as the root user an
 
 # Get and verify username and password.
 getuserandpass || error "User exited."
-
-# Give warning if user already exists.
-usercheck || error "User exited."
 
 ### The rest of the script requires no user input.
 
@@ -155,6 +141,3 @@ chsh -s /usr/bin/fish $name >/dev/null 2>&1
 # serveral important commands, `shutdown`, `reboot`, updating, etc. without a password.
 newperms "%wheel ALL=(ALL) ALL #DARBS
 %wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/packer -Syu,/usr/bin/packer -Syyu,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/yay,/usr/bin/pacman -Syyuw --noconfirm"
-
-# Last message! Install complete!
-finalize
