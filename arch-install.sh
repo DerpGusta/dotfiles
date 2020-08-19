8e176d75daa72bdd8418fcea2490d1ad17f9d73b
DerpGusta <derp@derp.com>
2020-08-19 16:13:31 +0530

    awesome

--- arch-install.sh
+++ arch-install.sh
@@ -1,22 +1,11 @@
 #!/usr/bin/env bash
 
 if [[ ! $(curl -Is http://www.google.com/ | head -n 1) =~ "200 OK" ]]; then
-  echo "Your Internet seems broken. Press Ctrl-C to abort or enter to continue."
+  echo -e "\e[1;32mYour Internet seems broken. Press Ctrl-C to abort or enter to continue.\e[0m"
   read
 fi
 
-read -rsp 'Enter password for root:' rootpass
-echo
 name="derp"
-read -rsp 'Enter a password for user.' pass1
-echo
-read -rsp 'Retype password.' pass2
-echo
-while ! [ "$pass1" = "$pass2" ]; do
-    unset pass2
-    read -rsp 'Enter a password for that user.' pass1
-    read -rsp 'Retype password.' pass2
-done 
 chroot='arch-chroot /mnt /bin/bash'
 chcmd='arch-chroot /mnt'
 
@@ -37,9 +26,9 @@ swapon /dev/sda3
 
 pacman -S --noconfirm pacman-contrib
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.orig
-echo -e "\n Selecting the fastest servers \n"
+echo -e "\e[1;32m\n Selecting the fastest servers \n\e[0m"
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.orig >/etc/pacman.d/mirrorlist
-echo -e "\n Updating packages"
+echo -e "\e[1;32m\n Updating packages\e[0m"
 pacman -Sy
 
 pacstrap /mnt base base-devel linux linux-firmware i3-gaps \
@@ -48,18 +37,18 @@ cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/
 
 genfstab -p /mnt >>/mnt/etc/fstab
 root_uuid="$(lsblk /dev/sda2 -no uuid)"
-echo "Adding user now"
+echo -e "\e[1;32mAdding user now\e[0m"
 $chcmd groupadd "$name" >/dev/null 2>&1
 arch-chroot /mnt useradd -m -g wheel -s /bin/bash "$name" >/dev/null
 arch-chroot /mnt usermod -a -G audio,video
-arch-chroot "echo \"$name:$pass1\" | chpasswd"
-unset pass1 pass2
+arch-chroot /mnt echo -e "root:toor" | chpasswd
+arch-chroot /mnt echo -e "$name:arch" | chpasswd
 
-echo -e "\n REFRESHING ARCHLINUX KEYRING"
+echo -e "\e[1;32m\n REFRESHING ARCHLINUX KEYRING\e[0m"
 $chcmd pacman --noconfirm -Sy archlinux-keyring >/dev/null 2>&1
 $chmod -c '[[ -f /etc/sudoers ]] && cp "/etc/sudoers" "/etc/sudoers.bak"'
 
-echo -e "\n Making changes to sudoers"
+echo -e "\e[1;32m\n Making changes to sudoers\e[0m"
 cat >> "/mnt/etc/sudoers" << EOF
 Defaults        env_reset
 Defaults        mail_badpass
@@ -72,7 +61,7 @@ root ALL=(ALL:ALL) ALL
 @includedir /etc/sudoers.d
 EOF
 
-echo -e "\n Installing yay and installing dotfiles"
+echo -e "\e[1;32m\n Installing yay and installing dotfiles\e[0m"
 $chroot << EOF
 cd /tmp
 rm -rf /tmp/yay-bin*
@@ -80,15 +69,16 @@ curl -sO https://aur.archlinux.org/cgit/aur.git/snapshot/yay-bin.tar.gz &&
 sudo -u "$name" tar -xvf yay-bin.tar.gz >/dev/null 2>&1 && cd yay-bin
 sudo -u "$name" makepkg --noconfirm -sirc >/dev/null 2>&1
 cd /tmp
-sudo -u "$name" git clone --depth 1 https://github.com/DerpGusta/dotfiles.git  /home/derp/dotfiles >/dev/null 2>&1
-cd "/home/derp/dotfiles/"
+yay -Sy
+sudo -u "$name" git clone --depth 1 https://github.com/DerpGusta/dotfiles.git  /home/$user/dotfiles >/dev/null 2>&1
+cd "/home/$user/dotfiles/"
 dirs=(*/)
 sudo -u "$name" stow "${dirs[@]%/}"
 grep "^Color" /etc/pacman.conf >/dev/null || sed -i "s/^#Color$/Color/" /etc/pacman.conf
 sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
 EOF
 
-echo -e "\n Setting up timezone, bootloader and startup services"
+echo -e "\e[1;32m\n Setting up timezone, bootloader and startup services\e[0m"
 
 $chroot <<EOF
 locale-gen
@@ -110,33 +100,34 @@ BOOTEND
 
 echo "console-mode 1" >> /mnt/boot/loader/loader.conf
 
-echo -e "\n Installing packages from packages.csv"
-ls /mnt/home/derp/dotfiles/
+echo -e "\e[1;32m\n Installing packages from packages.csv\e[0m"
+ls /mnt/home/$user/dotfiles/
 
-progsfile="/mnt/home/derp/dotfiles/packages.csv"
+progsfile="/mnt/home/$user/dotfiles/packages.csv"
 installpkg(){ $chcmd pacman --noconfirm --needed -S "$1" >/dev/null 2>&1 ;}
 
 maininstall() { # Installs all needed programs from main repo.
-	echo -e "\n Installing $1 -> $2 from pacman"
+	echo -e "\e[1;32m\n Installing $1 -> $2 from pacman\e[0m"
 	installpkg "$1"
 	}
 
 aurinstall() { \
-	echo -e "\n Installing $1 -> $2 from AUR"
+	echo -e "\e[1;32m\n Installing $1 -> $2 from AUR\e[0m"
     arch-chroot /mnt sudo -u "$name" yay -S --noconfirm "$1" >/dev/null 2>&1
 }
 installationloop() { \
-	([ -f "$progsfile" ] && cp "$progsfile" /mnt/home/derp/dotfiles/progs.csv) || echo "Where's the damn progsfile?" | sed '/^#/d' | eval grep "^[PGA]*," > /mnt/home/derp/dotfiles/progs.csv
+	([ -f "$progsfile" ] && cp "$progsfile" /mnt/home/$user/dotfiles/progs.csv) || echo -e "\e[1;32mWhere's the damn progsfile?\e[0m" | sed '/^#/d' | eval grep "^[PGA]*," > /mnt/home/$user/dotfiles/progs.csv
 	while IFS=, read -r tag program comment; do
 		n=$((n+1))
-		echo "$comment" | grep "^\".*\"$" >/dev/null 2>&1 && comment="$(echo "$comment" | sed "s/\(^\"\|\"$\)//g")"
+		echo -e "\e[1;32m$comment\e[0m" | grep "^\".*\"$" >/dev/null 2>&1 && comment="$(echo -e "\e[1;32m$comment\[e0m" | sed "s/\(^\"\|\"$\)//g")"
 		case "$tag" in
 			"A") aurinstall "$program" "$comment" ;;
 			*) maininstall "$program" "$comment" ;;
 		esac
-	done < /mnt/home/derp/dotfiles/progs.csv ;}
+	done < /mnt/home/$user/dotfiles/progs.csv ;}
 installationloop
-
-echo -e "\n Unmounting all the partitons"
+$chcmd sudo -u "$user" ./home/$user/dotfiles/stow.sh
+echo -e "\e[1;32m\n Unmounting all the partitons\e[0m"
 umount -R /mnt
 
+echo -e "\e[1;31mDONT FORGET UPDATING YOUR DEFAULT PASSWORDS!\e[0m"
---
+++ stow.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ~/dotfiles
+stow -v i3 alacritty Xresources fish nvim polybar rofi sxiv tmux 
