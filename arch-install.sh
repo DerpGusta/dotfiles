bba75deb9eff992130d17f6e7397e5f06ea62e92
Sri harsha <40120124+DerpGusta@users.noreply.github.com>
2020-08-20 13:49:35 +0530

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -6,14 +6,19 @@ if [[ ! $(curl -Is http://www.google.com/ | head -n 1) =~ "200 OK" ]]; then
 fi
 
 name="derp"
-chroot='arch-chroot /mnt /bin/bash'
-chcmd='arch-chroot /mnt'
 
 timedatectl set-ntp true
+wipefs -a /dev/sda1
+wipefs -a /dev/sda2
+wipefs -a /dev/sda3
+wipefs -a /dev/sda
 sgdisk -o /dev/sda
 sgdisk /dev/sda -n 1::+512M -t 1:ef00 #EFI
 sgdisk /dev/sda -n 2::-512M -t 2:8304 #root
 sgdisk /dev/sda -n 3 -t 3:8200 #swap
+wipefs -a /dev/sda1
+wipefs -a /dev/sda2
+wipefs -a /dev/sda3
 
 mkfs.vfat -F32 /dev/sda1
 mkfs.ext4 /dev/sda2
@@ -26,29 +31,36 @@ swapon /dev/sda3
 
 pacman -S --noconfirm pacman-contrib
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.orig
+
 echo -e "\e[1;32m\n Selecting the fastest servers \n\e[0m"
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.orig >/etc/pacman.d/mirrorlist
+
 echo -e "\e[1;32m\n Updating packages\e[0m"
 pacman -Sy
 
+echo -e "\e[1;32\nInstalling base packages\e[0m"
 pacstrap /mnt base base-devel linux linux-firmware i3-gaps \
 open-vm-tools lxdm stow xf86-video-vmware xf86-input-vmmouse
-cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/ 
+
+cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+arch-chroot /mnt pacman -Sy
 
 genfstab -p /mnt >>/mnt/etc/fstab
 root_uuid="$(lsblk /dev/sda2 -no uuid)"
+
 echo -e "\e[1;32mAdding user now\e[0m"
-$chcmd groupadd "$name" >/dev/null 2>&1
-arch-chroot /mnt useradd -m -g wheel -s /bin/bash "$name" >/dev/null
+arch-chroot /mnt useradd -m -g wheel,users -s /bin/bash "$name" >/dev/null
 arch-chroot /mnt usermod -a -G audio,video $name
-echo "root:toor" | chpasswd -R /mnt 
+arch-chroot /mnt sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+echo "root:root" | chpasswd -R /mnt 
 echo "derp:arch" | chpasswd -R /mnt
 
 echo -e "\e[1;32m\n REFRESHING ARCHLINUX KEYRING\e[0m"
-$chcmd pacman --noconfirm -Sy archlinux-keyring >/dev/null 2>&1
+arch-chroot /mnt pacman --noconfirm -Sy archlinux-keyring
 
 echo -e "\e[1;32m\n Installing yay and installing dotfiles\e[0m"
-$chroot << EOF
+arch-chroot /mnt /bin/bash << EOF
+name="derp"
 cd /tmp
 rm -rf /tmp/yay-bin*
 curl -sO https://aur.archlinux.org/cgit/aur.git/snapshot/yay-bin.tar.gz &&
@@ -58,21 +70,23 @@ cd /tmp
 yay -Sy
 sudo -u "$name" git clone --depth 1 https://github.com/DerpGusta/dotfiles.git  /home/$name/dotfiles >/dev/null 2>&1
 cd "/home/$name/dotfiles/"
-dirs=(*/)
-sudo -u "$name" stow "${dirs[@]%/}"
+sudo -u "$name" dirs=(*/); stow "${dirs[@]%/}"
 grep "^Color" /etc/pacman.conf >/dev/null || sed -i "s/^#Color$/Color/" /etc/pacman.conf
 sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
 EOF
 
 echo -e "\e[1;32m\n Setting up timezone, bootloader and startup services\e[0m"
 
-$chroot <<EOF
+cp /etc/systemd/network/20-ethernet.network /mnt/etc/systemd/network/
+sed -i '/^\[Network\]/a DNS=1.0.0.1/' /mnt/etc/systemd/network/20-ethernet.network
+arch-chroot /mnt /bin/bash <<EOF
 locale-gen
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 bootctl install
 rmmod pcspkr
 echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
 systemctl enable systemd-networkd
+systemctl enable systemd-resolved
 systemctl enable vmware-vmblock-fuse.service vmtoolsd.service
 systemctl enable lxdm.service
 EOF
@@ -90,7 +104,7 @@ echo -e "\e[1;32m\n Installing packages from packages.csv\e[0m"
 ls /mnt/home/$name/dotfiles/
 
 progsfile="/mnt/home/$name/dotfiles/packages.csv"
-installpkg(){ $chcmd pacman --noconfirm --needed -S "$1" >/dev/null 2>&1 ;}
+installpkg(){ arch-chroot /mnt pacman --noconfirm --needed -S "$1" >/dev/null 2>&1 ;}
 
 maininstall() { # Installs all needed programs from main repo.
 	echo -e "\e[1;32m\n Installing $1 -> $2 from pacman\e[0m"
@@ -99,7 +113,7 @@ maininstall() { # Installs all needed programs from main repo.
 
 aurinstall() { \
 	echo -e "\e[1;32m\n Installing $1 -> $2 from AUR\e[0m"
-    arch-chroot /mnt sudo -u "$name" yay -S --noconfirm "$1" >/dev/null 2>&1
+    arch-chroot /mnt sudo -u "$name" yay -S --noconfirm "$1"
 }
 installationloop() { \
 	([ -f "$progsfile" ] && cp "$progsfile" /mnt/home/$name/dotfiles/progs.csv) || echo -e "\e[1;32mWhere's the damn progsfile?\e[0m" | sed '/^#/d' | eval grep "^[PGA]*," > /mnt/home/$name/dotfiles/progs.csv
@@ -112,7 +126,8 @@ installationloop() { \
 		esac
 	done < /mnt/home/derp/dotfiles/progs.csv ;}
 installationloop
-$chcmd sudo -u "$name" ./home/$name/dotfiles/stow.sh
+arch-chroot /mnt sudo -u "$name" ./home/$name/dotfiles/stow.sh
+
 echo -e "\e[1;32m\n Unmounting all the partitons\e[0m"
 umount -R /mnt
 
